609bb2fc9af2b3f21f83e99566ec7469d9405d49
SebaPrado <sebastian.pradomelesi@gmail.com>
2025-05-28 10:26:14 +0200

    3

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild'
+  },
+  base: './',
   server: {
     host: "0.0.0.0",
     port: 5173,
